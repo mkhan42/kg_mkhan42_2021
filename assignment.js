936be9988a54ffc5014ba5faea6b91fe636a7374
mkhan42 <michallekhan9@gmail.com>
2021-05-01 22:41:09 -0400

    functionality of work conversion complete... Still need to complete user response section

--- assignment.js
+++ assignment.js
@@ -1,14 +1,43 @@
-let numBase = {
-    0:"Zero"
-    1: "One",
-    2: "Two",
-    3: "Three",
-    4: "Four",
-    5: "Five",
-    6: "Six",
-    7: "Seven",
-    8: "Eight",
-    9: "Nine"
-}
+const prompt = require('prompt-sync')();
+const response = prompt('Enter in n');
+console.log(response)
+// let numbers = [10,34,85]
 
-let testArray = new Array();
+const numToString = (response) => {
+    let numBase = {
+        "0": "Zero",
+        "1": "One",
+        "2": "Two",
+        "3": "Three",
+        "4": "Four",
+        "5": "Five",
+        "6": "Six",
+        "7": "Seven",
+        "8": "Eight",
+        "9": "Nine"
+    }
+    
+    let stringArr = []
+    
+    array.forEach((element, i) => {
+        let stringNum = element.toString().split('')
+        let numWord = ''
+    
+        stringNum.forEach((num) => {
+            for(let digit in numBase) {
+                if(digit == num) {
+                    numWord = numWord + numBase[digit]
+                }
+            }
+    
+        })
+        stringArr.push(numWord)
+    })
+    
+    let finalString = stringArr.join(", ")
+    
+    return finalString
+    
+    
+
+}
